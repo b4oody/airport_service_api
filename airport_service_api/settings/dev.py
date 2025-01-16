14b0b2d449097cfa6bf3c6e957be1eb831fe99bf
VR <vladrymarchuk@gmail.com>
2025-01-16 22:45:48 +0200

    feat: initial project setup, add env, change db on postgresql
       - Created new Django project
       - Split settings.py into base, prod, and dev modules
       - Added .env file for environment variables
       - Configured PostgreSQL as the database backend

---
+++ airport_service_api/settings/dev.py
@@ -0,0 +1,6 @@
+from airport_service_api.settings.base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
---
+++ airport_service_api/settings/prod.py
@@ -0,0 +1,5 @@
+from airport_service_api.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = []
