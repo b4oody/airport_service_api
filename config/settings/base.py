994f70261ae0da4ad8d90385e98559c2af1e32ec
VR <vladrymarchuk@gmail.com>
2025-01-17 02:28:11 +0200

    feat: add app in base.py, add auth_user_model, makemigrations and migrate for user app

--- config/settings/base.py
+++ config/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "air_service",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
