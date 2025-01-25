ca62b5823c00ca48857a2ab4b27ec26baa8840d1
VR <vladrymarchuk@gmail.com>
2025-01-26 00:33:42 +0200

    feat: implement pagination

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,3 +128,7 @@ AUTH_USER_MODEL = "user.User"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 15
+}
