14f19e05c41fc9fb824dba72a2505338348116f7
VR <vladrymarchuk@gmail.com>
2025-01-26 23:51:34 +0200

    feat: add TokenAuthentication in settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "user",
     "debug_toolbar",
     "django_filters",
+    "rest_framework.authtoken",
 
 ]
 
@@ -83,7 +84,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -129,6 +129,9 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 15
 }
