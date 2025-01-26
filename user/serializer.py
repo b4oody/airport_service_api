1394115cb8f89a4a7598f18721eb4c6948e55700
VR <vladrymarchuk@gmail.com>
2025-01-27 01:52:05 +0200

    feat: add transaction in serializer user

--- user/serializer.py
+++ user/serializer.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db import transaction
 from rest_framework import serializers
 
 
@@ -9,10 +10,12 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
+    @transaction.atomic
     def create(self, validated_data):
         """Create User with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    @transaction.atomic
     def update(self, instance, validated_data):
         """Update User with encrypted password."""
         password = validated_data.pop("password", None)
